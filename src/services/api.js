cc98bfbba211c6d03016c19718ce84069ee0a156
aylinka7 <ayladylova@gmail.com>
2025-12-10 22:16:40 +0600

    vercel configuration

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const API_BASE = import.meta.env.VITE_API_BASE;
+const API_BASE = 'http://localhost:8080/api';
+// const API_BASE = import.meta.env.VITE_API_BASE;
 
 
 export const getCourses = async () => {
